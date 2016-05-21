6d746b0d412fb0ef5ee6867cb373dd0ea97c99d6
snabeljoel <snabeljoel@gmail.com>
2016-05-21 10:16:03 +0200

    Add files via upload
    Optimize the appearance scanning by scanning unique pattern only one time.

--- main.lua
+++ main.lua
@@ -1,25 +1,31 @@
 
--- remove empty slots
+-- Removes empty slots that has no sample data, loaded plugin or midi output
 
 function instrument_is_empty(instrument)
- local inst = renoise.song():instrument(instrument)
- local has_sample_data = false
+
+ local inst, has_sample_data = renoise.song():instrument(instrument), false
  for _, sample in ipairs(inst.samples) do
   has_sample_data = has_sample_data or sample.sample_buffer.has_sample_data
  end
  if inst.name ~= "" or inst.plugin_properties.plugin_loaded or inst.midi_output_properties.device_name ~= "" or has_sample_data then return false else return true end
+
 end
+
+
 function remove_empty_slots()
+
  for instrument in ripairs(renoise.song().instruments) do
   if instrument_is_empty(instrument) and #renoise.song().instruments > 1 then
    renoise.song().delete_instrument_at(renoise.song(),instrument)
   end 
  end
+
 end
 
--- generic instrument list manipulation used by the sorting methods below
+-- Sort instruments according to a mapping definition
 
 function sort_instruments(new_mapping)
+
  renoise.app():show_status("Remapping instruments ...")
  table.sort(new_mapping, function(a, b) return a.original_position < b.original_position end)
  local doing_pos = 1
@@ -31,11 +37,13 @@ function sort_instruments(new_mapping)
  new_mapping[doing_pos], new_mapping[new_mapping[doing_pos].new_position] = new_mapping[new_mapping[doing_pos].new_position], new_mapping[doing_pos]
  end
  renoise.app():show_status("")
+
 end
 
--- returns a mapping definition sorted alphabetically by instrument name
+-- Returns a mapping definition sorted alphabetically by instrument name
 
 function map_alphabetic()
+
  local map_table = { }
  for i, instrument in ipairs(renoise.song().instruments) do table.insert(map_table, { name = instrument.name, original_position = i }) end
  table.sort(map_table, function(a, b)
@@ -46,11 +54,13 @@ function map_alphabetic()
   map_table[instrument].new_position = instrument
  end
  return map_table
+ 
 end
 
--- returns a reversed mapping definition
+-- Returns a reversed mapping definition
 
 function map_reverse()
+
  local map_table = { }
  for _, instrument in ipairs(renoise.song().instruments) do
   table.insert(map_table, 1, { name = instrument.name, original_position = _ })
@@ -59,11 +69,13 @@ function map_reverse()
   map_table[instrument].new_position = instrument
  end
  return map_table
+ 
 end
 
--- returns a mapping definition sorted by sample(s) size in instrument
+-- Returns a mapping definition sorted by sample(s) size in instrument
 
 function size_of_instrument(instrument)
+
  local bitsize = 0
  for _, sample in ipairs(renoise.song():instrument(instrument).samples) do
   if sample.sample_buffer.has_sample_data then
@@ -87,17 +99,19 @@ function map_size()
   map_table[instrument].new_position = instrument
  end
  return map_table
+ 
 end
 
--- returns a mapping definition sorted by appearance in song (scanned horizonally)
+-- Returns a mapping definition sorted by appearance in song (scanned horizonally)
 
 function map_appearance_song()
- local first_hit = { }
- local note_cell = 0
- local map_table = { } 
+
+ local first_hit, note_cell, map_table, scanned_patterns = { }, 0, { }, { }
  for _, instrument in ipairs(renoise.song().instruments) do table.insert(map_table, { name = instrument.name, original_position = _ }) end
  renoise.app():show_status("Scanning song ...")
  for sequence, pattern_index in ipairs(renoise.song().sequencer.pattern_sequence) do
+  if scanned_patterns[pattern_index] then else
+  scanned_patterns[pattern_index] = true 
   local pattern = renoise.song():pattern(pattern_index)
   for line = 1, pattern.number_of_lines do
    for track = 1, renoise.song().sequencer_track_count do
@@ -115,7 +129,7 @@ function map_appearance_song()
        end
       end
      end
-    end end end end end end
+    end end end end end end end
  for pos in ipairs(map_table) do
   if not map_table[pos].note_cell then map_table[pos].note_cell = math.huge end
  end
@@ -128,18 +142,20 @@ function map_appearance_song()
  end
  renoise.app():show_status("")
  return map_table
+ 
 end
 
--- returns a mapping definition sorted by appearance in song (scanned track by track)
+-- Returns a mapping definition sorted by appearance in song (scanned track by track)
 
 function map_appearance_track()
- local first_hit = { }
- local hit_order = 0
- local map_table = { }  
+
+ local first_hit, hit_order, map_table, scanned_patterns = { }, 0, { }, { }
  for _, instrument in ipairs(renoise.song().instruments) do table.insert(map_table, { name = instrument.name, original_position = _, first_hit = math.huge }) end 
 renoise.app():show_status("Scanning song ...")
 for track_index = 1, renoise.song().sequencer_track_count do
  for seq, pattern_index in ipairs(renoise.song().sequencer.pattern_sequence) do
+  if scanned_patterns[pattern_index] then else
+  scanned_patterns[pattern_index] = true
   local track = renoise.song():pattern(pattern_index):track(track_index)
   if not track.is_empty and renoise.song():track(track_index).type == renoise.Track.TRACK_TYPE_SEQUENCER then
    for line_index, line in ipairs(track.lines) do
@@ -152,7 +168,7 @@ for track_index = 1, renoise.song().sequencer_track_count do
       if map_table[val.instrument_value+1].first_hit > hit_order then 
        hit_order = hit_order + 1
        map_table[val.instrument_value+1].first_hit = hit_order
-end end end end end end end end end end
+end end end end end end end end end end end
  table.sort(map_table, function(a, b)
   if a.first_hit == b.first_hit then return a.original_position < b.original_position
   else return a.first_hit < b.first_hit end  
@@ -162,11 +178,13 @@ end end end end end end end end end end
  end
  renoise.app():show_status("")
  return map_table 
+ 
 end
 
--- returns a randomized mapping definition
+-- Returns a randomized mapping definition
 
 function map_randomize()
+
  local map_table = { }
  for _, instrument in ipairs(renoise.song().instruments) do table.insert(map_table, { name = instrument.name, original_position = _, random = math.random() }) end
  table.sort(map_table, function(a, b) return a.random < b.random end)
@@ -174,11 +192,13 @@ function map_randomize()
   map_table[instrument].new_position = instrument
  end
  return map_table
+ 
 end
 
--- returns a mapping definition sorted by most used instrument
+-- Returns a mapping definition sorted by most used instrument
 
 function map_most_used()
+
  local map_table = { }
  for _, instrument in ipairs(renoise.song().instruments) do table.insert(map_table, { name = instrument.name, original_position = _, population = 0 }) end
  renoise.app():show_status("Scanning song ...")
@@ -203,46 +223,48 @@ end end end end end end end end
  end
  renoise.app():show_status("")
  return map_table
+ 
 end
 
 -- Menu entries
 
 renoise.tool():add_menu_entry {
-  name = "Instrument Box:Sort Instruments ...:Name",
+  name = "Instrument Box:Sort Instruments...:Name",
   invoke = function() sort_instruments(map_alphabetic()) end
 }
 
 renoise.tool():add_menu_entry {
-  name = "Instrument Box:Sort Instruments ...:Size",
+  name = "Instrument Box:Sort Instruments...:Size",
   invoke = function() sort_instruments(map_size()) end
 }
 
 renoise.tool():add_menu_entry {
-  name = "Instrument Box:Sort Instruments ...:Most Used",
+  name = "Instrument Box:Sort Instruments...:Most Used",
   invoke = function() sort_instruments(map_most_used()) end
 }
 
 renoise.tool():add_menu_entry {
-  name = "Instrument Box:Sort Instruments ...:Appearance (from start)",
+  name = "Instrument Box:Sort Instruments...:Appearance (from start)",
   invoke = function() sort_instruments(map_appearance_song()) end
 }
 
 renoise.tool():add_menu_entry {
-  name = "Instrument Box:Sort Instruments ...:Appearance (from track)",
+  name = "Instrument Box:Sort Instruments...:Appearance (from track)",
   invoke = function() sort_instruments(map_appearance_track()) end
 }
 
 renoise.tool():add_menu_entry {
-  name = "Instrument Box:Sort Instruments ...:Randomize",
+  name = "Instrument Box:Sort Instruments...:Randomize",
   invoke = function() sort_instruments(map_randomize()) end
 }
 
 renoise.tool():add_menu_entry {
-  name = "Instrument Box:Sort Instruments ...:Reverse",
+  name = "Instrument Box:Sort Instruments...:Reverse",
   invoke = function() sort_instruments(map_reverse()) end
 }
 
 renoise.tool():add_menu_entry {
-  name = "---Instrument Box:Sort Instruments ...:Remove empty slots",
+  name = "---Instrument Box:Sort Instruments...:Remove empty slots",
   invoke = function() remove_empty_slots() end
 }
+
